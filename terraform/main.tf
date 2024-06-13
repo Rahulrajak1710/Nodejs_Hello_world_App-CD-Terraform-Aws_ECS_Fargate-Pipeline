fc3e8c76a488880f8c5fe424b2e234cf3c381084
Rahul <67949307+Rahulrajak1710@users.noreply.github.com>
2024-06-13 12:59:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "terra" {
   bucket = "my-terraform-bucket-rahulrajak"
   acl    = "public-read-write"
 }
